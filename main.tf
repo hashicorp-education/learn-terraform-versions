f8448c250aeaed1b380d1168e05709c7a1610b39
sraman-hash <sridevi.raman@hashicorp.com>
2024-10-21 15:13:42 -0500

    Upgrade version constraint to Terraform 0.13

--- main.tf
+++ main.tf
@@ -1,6 +1,17 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
+terraform {
+  required_providers {
+    aws = {
+      version = "~> 4.0"
+    }
+    random = {
+      version = ">= 2.1.2"
+    }
+  }
 
+  required_version = "~> 0.13.0"
+}
 provider "aws" {
   region = "us-west-2"
 }
